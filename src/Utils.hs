e76e304ef985be1aa03bce3982255214a1d59c20
Paul Kinsky <pkinsky@gmail.com>
2014-08-15 13:10:11 -0400

    hlint utils

--- src/Utils.hs
+++ src/Utils.hs
@@ -1,4 +1,4 @@
-module Utils(shuffle, rnd_select, qwertyToDvorak)  where
+module Utils(rnd_select, qwertyToDvorak)  where
 
 import System.Random
 import Data.Map (Map)
@@ -7,29 +7,11 @@ import Control.Monad
 import Data.Array.IO
 
 
--- src: http://www.haskell.org/haskellwiki/Random_shuffle
- 
--- | Randomly shuffle a list
---   /O(N)/
-shuffle :: [a] -> IO [a]
-shuffle xs = do
-        ar <- newArray n xs
-        forM [1..n] $ \i -> do
-            j <- randomRIO (i,n)
-            vi <- readArray ar i
-            vj <- readArray ar j
-            writeArray ar j vi
-            return vj
-  where
-    n = length xs
-    newArray :: Int -> [a] -> IO (IOArray Int a)
-    newArray n xs =  newListArray (1,n) xs
-
 
 -- taken from http://www.haskell.org/haskellwiki/index.php?title=99_questions/Solutions/23
 rnd_select xs n = do
     gen <- getStdGen
-    return $ take n [ xs !! x | x <- randomRs (0, (length xs) - 1) gen]
+    return $ take n [ xs !! x | x <- randomRs (0, length xs - 1) gen]
 
 
 -- thank you mysterious pastebin stranger (code from http://pastebin.com/wYSrKB6z)
